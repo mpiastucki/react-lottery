2c65423ce7b332d8d64f351447d7a64578a2406d
mpiastucki <michael.piastucki@woven-planet.global>
2022-09-12 10:26:39 +0900

    change build folder

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.use("../dist");
+app.use("../build");
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
