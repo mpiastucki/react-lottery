8807e6f4938804d41ce1c7fd3d02dd666b5f57a9
mpiastucki <michael.piastucki@woven-planet.global>
2022-09-12 10:25:58 +0900

    add express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use("../dist");
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
