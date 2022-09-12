9cc6f1e6dd939074cdbdd8e41e8ebbe7e21e190b
mpiastucki <michael.piastucki@woven-planet.global>
2022-09-12 10:34:23 +0900

    update express static

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
+const path=require('path')
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.use("../build");
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
